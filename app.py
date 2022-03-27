6adbedd17a6b9de273c36e3b2364c0ae9ee367c5
simon <simon.matuschek.89@googlemail.com>
2022-03-27 13:21:28 +0200

    app for heroku

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ from models import RootUsers
 app = Flask(__name__)
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:peterlustig@localhost/rootsrock'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:peterlustig@localhost/rootsrock'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kjlgqrfqnbicyn:c26d248de8e9f7ecb1556827592c49922c8c47631804f8a0ad1ccea8881ebb12@ec2-52-201-124-168.compute-1.amazonaws.com:5432/d3qhna99gf3vpe'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db_init(app)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
